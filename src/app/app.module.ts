b88fc4711c6b3e8e0adb1edee672014a5e21a862
Gilles <gilles.ferrand31@gmail.com>
2022-02-19 20:39:51 +0100

    Install NGRX & devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {environment} from "../environments/environment";
+import {StoreDevtoolsModule} from "@ngrx/store-devtools";
+import {StoreModule} from "@ngrx/store";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
