4d9f2310fac33011bece1aff8e74a3947cbcc4a5
Gilles <gilles.ferrand31@gmail.com>
2022-02-19 21:08:29 +0100

    Install lint-staged + prettier everything

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,18 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
-import {environment} from "../environments/environment";
-import {StoreDevtoolsModule} from "@ngrx/store-devtools";
-import {StoreModule} from "@ngrx/store";
+import {environment} from '../environments/environment';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {StoreModule} from '@ngrx/store';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    StoreModule.forRoot({}),
-    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
-    BrowserModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [
+        StoreModule.forRoot({}),
+        StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+        BrowserModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
